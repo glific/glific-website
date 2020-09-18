626050504f607348d239cc79b60f9ee078a85e17
mohitgusain <mohit.gusain@coloredcow.com>
2020-09-18 18:31:33 +0530

    changes

--- public/wp-content/themes/glific/template-coming-soon.php
+++ public/wp-content/themes/glific/template-coming-soon.php
@@ -6,7 +6,7 @@
  */
 get_header();
 ?>
-<div class="template-coming-soon  mt-21 pt-21  w-100p h-300 h-md-400 h-xl-600 d-flex justify-content-center align-items-center">
+<div class="template-coming-soon mt-27 pt-27  w-100p h-300 h-md-400 h-xl-600 d-flex justify-content-center align-items-center">
 	<h1 class="text-center  fz-42 font-heebo-bold">Page Coming Soon</h1>
 </div>
 

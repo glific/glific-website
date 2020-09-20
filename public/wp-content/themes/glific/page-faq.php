4d8b09a60bbbc64a779c7d0a93b423edea7446d0
satendra-sr <officialsatendrarawat@gmail.com>
2020-09-20 17:29:04 +0530

    Initial page faq

---
+++ public/wp-content/themes/glific/page-faq.php
@@ -0,0 +1,60 @@
+<?php
+
+/**
+ * File functions.php for theme glific
+ *
+ * @package glific
+ */
+
+get_header();
+
+$faqs = get_field('faq_content');
+$page_title = get_field('faq_page_title');
+?>
+
+
+<div class="bg-theme-white-smoke page-faq">
+    <div class="demo-section d-flex flex-column flex-md-row justify-content-md-between w-320 w-md-660 w-xl-880 mx-auto">
+        <div class="d-flex flex-column w-full justify-content-center pt-18 text-center">
+            <h3 class="text-theme-primary font-heebo-bold fz-28 fz-md-36 leading-40 mb-0"><?php echo $page_title; ?></h3>
+        </div>
+    </div>
+
+    <div class="pb-26 px-6 px-xl-31">
+        <?php foreach ($faqs as $key => $faq) { ?>
+        <div class="accordion py-4" id="accordionExample">
+            <div class="card rounded-20 card-shadow">
+                <div class="card-header bg-white rounded-20 p-0 card-shadow" id="headingOne">
+                    <button class="btn btn-link btn-block text-left rounded-20 px-6 text-decoration-none fz-24 font-heebo-regular text-theme-mine-shaft" type="button" data-toggle="collapse" data-target="#collapse<?php echo $key;?>" aria-expanded="false" aria-controls="collapse<?php echo $key;?>">
+                    <div class="d-flex flex-column flex-md-row">
+                        <div class="d-flex flex-row justify-content-between">
+                            <div class="fz-36 font-heebo-light my-auto pr-md-6 pr-xl-16">
+                                <?php echo strlen((string)$key) < 10 ? '0' . ($key + 1) : ($key + 1);?>
+                            </div>
+                            <div class="icon-dropdown d-md-none w-20 h-35 mt-4"> 
+                                <?php echo file_get_contents(get_template_directory(). '/dist/images/icon-arrow.svg') ?>
+                            </div>
+                        </div>
+                        <div class="my-auto py-6">
+                            <?php echo $faq['title']; ?>
+                        </div>
+                        <div class="icon-dropdown d-none d-md-block w-20 h-35 mt-4 ml-auto"> 
+                            <?php echo file_get_contents(get_template_directory(). '/dist/images/icon-arrow.svg') ?>
+                        </div>
+                    </div>
+                    </button>
+                </div>
+                <div id="collapse<?php echo $key;?>" class="collapse" aria-labelledby="headingOne" data-parent="#accordionExample">
+                    <div class="card-body fz-18 font-heebo-regular pl-xl-31 pr-xl-15 py-xl-15">
+                    <?php echo $faq['description']; ?>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <?php } ?>
+    </div>
+   
+</div>
+
+<?php
+get_footer();

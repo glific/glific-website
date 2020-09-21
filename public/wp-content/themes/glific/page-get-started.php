04719a290119fd3884d7b212d74f82e1cc5af880
kuldeep3 <kuldeepupreti3@gmail.com>
2020-09-21 10:36:23 +0530

    feedbacks resolved, added check on title and primary index

--- public/wp-content/themes/glific/page-get-started.php
+++ public/wp-content/themes/glific/page-get-started.php
@@ -18,15 +18,18 @@ $main_heading = get_field('main_heading');
     <div class="subsection-container d-flex flex-column ml-6 ml-xl-26 w-xl-936 w-md-712">
         <?php $primary_section_data = get_field('primary_section');
         if (!empty($primary_section_data)) :
-            foreach ($primary_section_data as $key => $data) : ?>
+            foreach ($primary_section_data as $key => $data) :
+                $primary_count_index = $key >= 9 ? '' : 0; ?>
                 <div class="flex-column flex-md-row d-flex justify-content-between mb-13.5 mb-xl-18 mb-md-20 w-full">
                     <div class="d-flex flex-row w-xl-67">
-                        <h2 class="font-heebo-light fz-60 text-theme-mine-shaft">0<?php echo $key + 1; ?></h2>
+                        <h2 class="font-heebo-light fz-60 text-theme-mine-shaft"><?php echo $primary_count_index; ?><?php echo $key + 1; ?></h2>
                     </div>
                     <div class="d-flex flex-column w-280 w-xl-770 w-md-613 rounded-30 bg-theme-gin get-started-content-box">
-                        <div class="bg-white p-6 pt-md-6 pb-md-4.5 rounded-20 pl-md-8.5">
-                            <h2 class="fz-24 font-heebo-regular text-theme-mine-shaft mb-0"><?php echo $data['title']; ?></h2>
-                        </div>
+                        <?php if (!empty($data['title'])) : ?>
+                            <div class="bg-white p-6 pt-md-6 pb-md-4.5 rounded-20 pl-md-8.5">
+                                <h2 class="fz-24 font-heebo-regular text-theme-mine-shaft mb-0"><?php echo $data['title']; ?></h2>
+                            </div>
+                        <?php endif; ?>
                         <div class="d-flex flex-column bg-theme-gin rounded-bottom-30 font-heebo-regular fz-18 leading-28 text-theme-mine-shaft pt-8 pb-6 pl-5 pb-md-6 pl-md-8">
                             <?php if (!empty($data['content'])) :
                                 foreach ($data['content'] as $content) : ?>

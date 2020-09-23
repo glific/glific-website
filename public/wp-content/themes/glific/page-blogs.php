f37955c9c2c22996b89e99b1ec721955e5989fbb
wevekamola <vivek.amola@coloredcow.in>
2020-09-23 19:34:51 +0530

    code updates

--- public/wp-content/themes/glific/page-blogs.php
+++ public/wp-content/themes/glific/page-blogs.php
@@ -5,74 +5,45 @@
  * @package glific
  */
 
-	get_header();
+get_header();
 ?>
-
 <div class="page-contact bg-theme-white-smoke pb-26">
-
 	<div class="pt-10 pt-md-16 pt-xl-18 pb-xl-4 pb-md-14 pb-10">
 		<h3 class="text-theme-primary font-heebo-bold fz-28 fz-md-36 leading-40 mb-0 text-center mb-6 mb-md-14 mb-xl-18">
 			Blogs
 		</h3>
 	</div>
-
+	<?php 
+		$wpb_all_query = new WP_Query(
+			array(
+				'post_type'=>'post', 
+				'post_status'=>'publish', 
+				'posts_per_page'=> -1
+			)
+		); 
+	
+	if ( $wpb_all_query->have_posts() ) : ?>
 	<div class="blogs-section mx-auto w-320 w-md-600 w-xl-1135 d-flex justify-content-center justify-content-md-between flex-wrap">
-
-		<div class="w-285 mb-12">
-			<div class="w-full h-169 bg-position-center bg-size-cover rounded-30" style="background-image: url('https://scontent.fdel41-1.fna.fbcdn.net/v/t1.0-9/60339781_2352237155053209_557709689593266176_o.jpg?_nc_cat=110&_nc_sid=6e5ad9&_nc_ohc=XJpVvgOqXK4AX_phtAY&_nc_ht=scontent.fdel41-1.fna&oh=e5e2ef1b93e5471f6ecdd825cb60ec37&oe=5F90BA7A');">
+		<?php while ( $wpb_all_query->have_posts() ) : $wpb_all_query->the_post(); ?>
+			<div class="w-285 mb-12">
+				<div class="w-full h-169 bg-position-center bg-size-cover rounded-30" style="background-image: url('https://scontent.fdel41-1.fna.fbcdn.net/v/t1.0-9/60339781_2352237155053209_557709689593266176_o.jpg?_nc_cat=110&_nc_sid=6e5ad9&_nc_ohc=XJpVvgOqXK4AX_phtAY&_nc_ht=scontent.fdel41-1.fna&oh=e5e2ef1b93e5471f6ecdd825cb60ec37&oe=5F90BA7A');">
+				</div>
+				<h5 class="font-heebo-medium fz-24 leading-xl-35 leading-26 mt-6">
+					Glific v0.3 takes off
+				</h5>
+				<p class="font-heebo-regular fz-18 leading-27 mt-6">
+					A new month, a new glific release. Really like the pace and duration of these sprints, makes the ..
+				</p>
+				<p class="font-heebo-regular fz-18 leading-27 mt-6">
+					SEPTEMBER 7, 2020
+				</p>
 			</div>
-			<h5 class="font-heebo-medium fz-24 leading-xl-35 leading-26 mt-6">
-				Glific v0.3 takes off
-			</h5>
-			<p class="font-heebo-regular fz-18 leading-27 mt-6">
-				A new month, a new glific release. Really like the pace and duration of these sprints, makes the ..
-			</p>
-			<p class="font-heebo-regular fz-18 leading-27 mt-6">
-				SEPTEMBER 7, 2020
-			</p>
-		</div>
-		
-		<div class="w-285 mb-12">
-			<div class="w-full h-169 bg-position-center bg-size-cover rounded-30" style="background-image: url('https://scontent.fdel41-1.fna.fbcdn.net/v/t1.0-9/60339781_2352237155053209_557709689593266176_o.jpg?_nc_cat=110&_nc_sid=6e5ad9&_nc_ohc=XJpVvgOqXK4AX_phtAY&_nc_ht=scontent.fdel41-1.fna&oh=e5e2ef1b93e5471f6ecdd825cb60ec37&oe=5F90BA7A');">
-			</div>
-			<h5 class="font-heebo-medium fz-24 leading-xl-35 leading-26 mt-6">
-				Glific v0.3 takes off
-			</h5>
-			<p class="font-heebo-regular fz-18 leading-27 mt-6">
-				A new month, a new glific release. Really like the pace and duration of these sprints, makes the ..
-			</p>
-			<p class="font-heebo-regular fz-18 leading-27 mt-6">
-				SEPTEMBER 7, 2020
-			</p>
-		</div>
-
-		<div class="w-285 mb-12">
-			<div class="w-full h-169 bg-position-center bg-size-cover rounded-30" style="background-image: url('https://scontent.fdel41-1.fna.fbcdn.net/v/t1.0-9/60339781_2352237155053209_557709689593266176_o.jpg?_nc_cat=110&_nc_sid=6e5ad9&_nc_ohc=XJpVvgOqXK4AX_phtAY&_nc_ht=scontent.fdel41-1.fna&oh=e5e2ef1b93e5471f6ecdd825cb60ec37&oe=5F90BA7A');">
-			</div>
-			<h5 class="font-heebo-medium fz-24 leading-xl-35 leading-26 mt-6">
-				Glific v0.3 takes off
-			</h5>
-			<p class="font-heebo-regular fz-18 leading-27 mt-6">
-				A new month, a new glific release. Really like the pace and duration of these sprints, makes the ..
-			</p>
-			<p class="font-heebo-regular fz-18 leading-27 mt-6">
-				SEPTEMBER 7, 2020
-			</p>
-		</div>
-
-		<div class="w-285 mb-12">
-			<div class="w-full h-169 bg-position-center bg-size-cover rounded-30" style="background-image: url('https://scontent.fdel41-1.fna.fbcdn.net/v/t1.0-9/60339781_2352237155053209_557709689593266176_o.jpg?_nc_cat=110&_nc_sid=6e5ad9&_nc_ohc=XJpVvgOqXK4AX_phtAY&_nc_ht=scontent.fdel41-1.fna&oh=e5e2ef1b93e5471f6ecdd825cb60ec37&oe=5F90BA7A');">
-			</div>
-			<h5 class="font-heebo-medium fz-24 leading-xl-35 leading-26 mt-6">
-				Glific v0.3 takes off
-			</h5>
-			<p class="font-heebo-regular fz-18 leading-27 mt-6">
-				A new month, a new glific release. Really like the pace and duration of these sprints, makes the ..
-			</p>
-			<p class="font-heebo-regular fz-18 leading-27 mt-6">
-				SEPTEMBER 7, 2020
-			</p>
-		</div>
+	    <?php 
+			endwhile; wp_reset_postdata(); 
+				else :
+					 _e( 'Sorry, no posts matched your criteria.' );
+			endif; 
+		?>
 	</div>
 
 	<div class="w-full d-flex justify-content-center">
@@ -80,7 +51,6 @@
 	</div>
 
 	<div class="mx-auto mt-20 w-90p w-md-737 w-xl-1010 mx-auto">
-
 		<div class="w-full w-md-full d-flex mb-9 mb-md-10 flex-column flex-md-row mx-auto justify-content-between">
 			<div class="source-code-section rounded-top-30 rounded-bottom-left-30 bg-theme-gin py-7 w-full px-10 w-md-361 h-287 w-xl-572 h-xl-257">
 				<h5 class="font-heebo-medium fz-24 leading-xl-35 leading-26">
@@ -115,11 +85,8 @@
 				</p>
 			</div>
 			<div class="newsletter-form d-flex w-full w-md-495 flex-column flex-xl-row">
-
 				<input type="text" class="border-0 bg-white rounded-12 px-7 w-full w-md-354 h-43" placeholder="Email">
-
 				<a class="mt-5 mt-xl-0 glific-button-border w-126 w-md-auto ml-auto bg-theme-secondary px-10 py-4 fz-18 leading-27 text-center text-white font-heebo-bold text-decoration-none" target="_blank" href="">Sign up</a>
-
 			</div>
 		</div>
 	</div>

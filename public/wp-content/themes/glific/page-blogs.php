b1f4e3e5322d95154966a8d747fc693a53c8634f
wevekamola <vivek.amola@coloredcow.in>
2020-09-23 17:05:47 +0530

    static ui completed

---
+++ public/wp-content/themes/glific/page-blogs.php
@@ -0,0 +1,130 @@
+<?php
+/**
+ * Blogs page for theme glific
+ *
+ * @package glific
+ */
+
+	get_header();
+?>
+
+<div class="page-contact bg-theme-white-smoke pb-26">
+
+	<div class="pt-10 pt-md-16 pt-xl-18 pb-xl-4 pb-md-14 pb-10">
+		<h3 class="text-theme-primary font-heebo-bold fz-28 fz-md-36 leading-40 mb-0 text-center mb-6 mb-md-14 mb-xl-18">
+			Blogs
+		</h3>
+	</div>
+
+	<div class="blogs-section mx-auto w-320 w-md-600 w-xl-1135 d-flex justify-content-center justify-content-md-between flex-wrap">
+
+		<div class="w-285 mb-12">
+			<div class="w-full h-169 bg-position-center bg-size-cover rounded-30" style="background-image: url('https://scontent.fdel41-1.fna.fbcdn.net/v/t1.0-9/60339781_2352237155053209_557709689593266176_o.jpg?_nc_cat=110&_nc_sid=6e5ad9&_nc_ohc=XJpVvgOqXK4AX_phtAY&_nc_ht=scontent.fdel41-1.fna&oh=e5e2ef1b93e5471f6ecdd825cb60ec37&oe=5F90BA7A');">
+			</div>
+			<h5 class="font-heebo-medium fz-24 leading-xl-35 leading-26 mt-6">
+				Glific v0.3 takes off
+			</h5>
+			<p class="font-heebo-regular fz-18 leading-27 mt-6">
+				A new month, a new glific release. Really like the pace and duration of these sprints, makes the ..
+			</p>
+			<p class="font-heebo-regular fz-18 leading-27 mt-6">
+				SEPTEMBER 7, 2020
+			</p>
+		</div>
+		
+		<div class="w-285 mb-12">
+			<div class="w-full h-169 bg-position-center bg-size-cover rounded-30" style="background-image: url('https://scontent.fdel41-1.fna.fbcdn.net/v/t1.0-9/60339781_2352237155053209_557709689593266176_o.jpg?_nc_cat=110&_nc_sid=6e5ad9&_nc_ohc=XJpVvgOqXK4AX_phtAY&_nc_ht=scontent.fdel41-1.fna&oh=e5e2ef1b93e5471f6ecdd825cb60ec37&oe=5F90BA7A');">
+			</div>
+			<h5 class="font-heebo-medium fz-24 leading-xl-35 leading-26 mt-6">
+				Glific v0.3 takes off
+			</h5>
+			<p class="font-heebo-regular fz-18 leading-27 mt-6">
+				A new month, a new glific release. Really like the pace and duration of these sprints, makes the ..
+			</p>
+			<p class="font-heebo-regular fz-18 leading-27 mt-6">
+				SEPTEMBER 7, 2020
+			</p>
+		</div>
+
+		<div class="w-285 mb-12">
+			<div class="w-full h-169 bg-position-center bg-size-cover rounded-30" style="background-image: url('https://scontent.fdel41-1.fna.fbcdn.net/v/t1.0-9/60339781_2352237155053209_557709689593266176_o.jpg?_nc_cat=110&_nc_sid=6e5ad9&_nc_ohc=XJpVvgOqXK4AX_phtAY&_nc_ht=scontent.fdel41-1.fna&oh=e5e2ef1b93e5471f6ecdd825cb60ec37&oe=5F90BA7A');">
+			</div>
+			<h5 class="font-heebo-medium fz-24 leading-xl-35 leading-26 mt-6">
+				Glific v0.3 takes off
+			</h5>
+			<p class="font-heebo-regular fz-18 leading-27 mt-6">
+				A new month, a new glific release. Really like the pace and duration of these sprints, makes the ..
+			</p>
+			<p class="font-heebo-regular fz-18 leading-27 mt-6">
+				SEPTEMBER 7, 2020
+			</p>
+		</div>
+
+		<div class="w-285 mb-12">
+			<div class="w-full h-169 bg-position-center bg-size-cover rounded-30" style="background-image: url('https://scontent.fdel41-1.fna.fbcdn.net/v/t1.0-9/60339781_2352237155053209_557709689593266176_o.jpg?_nc_cat=110&_nc_sid=6e5ad9&_nc_ohc=XJpVvgOqXK4AX_phtAY&_nc_ht=scontent.fdel41-1.fna&oh=e5e2ef1b93e5471f6ecdd825cb60ec37&oe=5F90BA7A');">
+			</div>
+			<h5 class="font-heebo-medium fz-24 leading-xl-35 leading-26 mt-6">
+				Glific v0.3 takes off
+			</h5>
+			<p class="font-heebo-regular fz-18 leading-27 mt-6">
+				A new month, a new glific release. Really like the pace and duration of these sprints, makes the ..
+			</p>
+			<p class="font-heebo-regular fz-18 leading-27 mt-6">
+				SEPTEMBER 7, 2020
+			</p>
+		</div>
+	</div>
+
+	<div class="w-full d-flex justify-content-center">
+		<a class="glific-button-border bg-theme-secondary w-190 px-6 py-4 fz-18 leading-27 text-center mx-auto text-white font-heebo-bold text-decoration-none" target="_blank" href="">Show More</a>
+	</div>
+
+	<div class="mx-auto mt-20 w-90p w-md-737 w-xl-1010 mx-auto">
+
+		<div class="w-full w-md-full d-flex mb-9 mb-md-10 flex-column flex-md-row mx-auto justify-content-between">
+			<div class="source-code-section rounded-top-30 rounded-bottom-left-30 bg-theme-gin py-7 w-full px-10 w-md-361 h-287 w-xl-572 h-xl-257">
+				<h5 class="font-heebo-medium fz-24 leading-xl-35 leading-26">
+					Source code
+				</h5>
+				<p class="font-heebo-regular fz-18 leading-27 mt-6 w-md-292 w-xl-460 mb-8">
+					We have each and every step documented to make future on-boarding easier. Go through our Github to get to know Glific thought it’s roots.
+				</p>
+				<a class="glific-button-border bg-theme-primary py-4 px-10 fz-18 leading-27 text-center text-white font-heebo-bold text-decoration-none" target="_blank" href="">
+					View Github
+				</a>
+			</div>
+			<div class="community-section rounded-top-30 rounded-bottom-right-30 bg-theme-gin py-7 px-10 w-full w-md-361 h-287 w-xl-421 h-xl-257 mt-10 mt-md-0">
+				<h5 class="font-heebo-medium fz-24 leading-xl-35 leading-26">
+					Community
+				</h5>
+				<p class="font-heebo-regular fz-18 leading-27 mt-6 w-md-310 mb-8">
+					Connect with us through our Open Source community on Discord. You can ask us question directly here.
+				</p>
+				<a class="glific-button-border bg-theme-primary py-4 px-10 fz-18 leading-27 text-center text-white font-heebo-bold text-decoration-none" target="_blank" href="">
+					View Discord
+				</a>
+			</div>
+		</div>
+		<div class="newsletter-section rounded-top-right-30 rounded-bottom-30 bg-theme-gin py-6 px-10 d-flex align-items-center justify-content-between w-full h-md-149 flex-column flex-md-row">
+			<div class="">
+				<h5 class="font-heebo-medium fz-24 leading-xl-35 leading-26">
+					Sign up for our newsletter
+				</h5>
+				<p class="font-heebo-regular fz-18 leading-27 mt-6 w-full w-md-335">
+					Enter your email here to receive our weekly newsletter about Glific’s Updates.
+				</p>
+			</div>
+			<div class="newsletter-form d-flex w-full w-md-495 flex-column flex-xl-row">
+
+				<input type="text" class="border-0 bg-white rounded-12 px-7 w-full w-md-354 h-43" placeholder="Email">
+
+				<a class="mt-5 mt-xl-0 glific-button-border w-126 w-md-auto ml-auto bg-theme-secondary px-10 py-4 fz-18 leading-27 text-center text-white font-heebo-bold text-decoration-none" target="_blank" href="">Sign up</a>
+
+			</div>
+		</div>
+	</div>
+</div>
+
+
+<?php
+	get_footer();

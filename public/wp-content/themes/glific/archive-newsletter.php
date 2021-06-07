f0cd5c9c0b8f61f0cab3120f9d6a0e91c1b31772
mdshamoon <mohdshamoon841@gmail.com>
2021-06-07 23:18:22 +0530

    added newsletter page

---
+++ public/wp-content/themes/glific/archive-newsletter.php
@@ -0,0 +1,33 @@
+<?php
+get_header();
+?>
+
+<div class="px-42 py-18 bg-theme-white-smoke">
+	<h2 class="pb-18 fz-36 font-weight-bold text-theme-primary">Newsletters</h2>
+	<?php
+	if ( have_posts() ) :
+		?>
+			<div class="row">
+			<?php
+			while ( have_posts() ) :
+				the_post();
+				?>		
+				<div class="col-md-4 ">
+				<a href="<?php echo get_field( 'link' ); ?>" class="text-decoration-none position-relative" target="_blank">					
+					<div class="px-8 py-10 bg-white h-full">
+					<h1 class="fz-36 font-weight-light text-theme-secondary pb-8"><?php echo get_field( 'date' ); ?></h1>
+					<div class="description fz-18 text-theme-pewter"><?php echo get_field( 'description' ); ?> </div></div>
+					</a>
+					<svg class="position-absolute left-36 bottom-20"  width="18px" focusable="false" viewBox="0 0 24 24" aria-hidden="true"><path d="M9 5v2h6.59L4 18.59 5.41 20 17 8.41V15h2V5H9z" fill="#119656"></path></svg>					
+				</div>	
+				<?php
+		endwhile;
+			?>
+		</div>
+		<?php
+	endif;
+	?>
+</div>
+<?php
+get_footer();
+
